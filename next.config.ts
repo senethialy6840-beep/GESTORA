ae223425466478b7146d014ddef7123e3c5c4779
Gestora Dev <contact@gestora.sn>
2026-09-01 13:55:15 +0000

    fix empty NEXTAUTH_URL

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
+if (process.env.NEXTAUTH_URL === "") {
+  delete process.env.NEXTAUTH_URL;
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
