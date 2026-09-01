3e0aa62a8ff005ab7cba547410162490555103d0
Gestora Dev <contact@gestora.sn>
2026-09-01 16:55:07 +0000

    security: ajout des headers HTTP et du fichier .env.example

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,42 @@ if (process.env.NEXTAUTH_URL === "") {
   delete process.env.NEXTAUTH_URL;
 }
 
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  }
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
