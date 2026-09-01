b794414149efe3dbe6e48cbfae0b66412b56e6b6
Gestora Dev <contact@gestora.sn>
2026-09-01 13:46:40 +0000

    fix prisma fallback

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL || "postgres://dummy:dummy@localhost/dummy";
 
 const pool = new Pool({
   connectionString,
